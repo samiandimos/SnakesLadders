fe5f0c1c703352dab9a5a3e20aca09cdf1d6662c
bruger <simortusos@gmail.com>
2018-12-21 17:49:19 +0100

    LoadingScreen working fine, with better transition score window with better layout and less memory consumption Fixed transition from endScreen to menuScreen fixed play again buttons when you try to play again
    if there is any issues , pls report it :)

---
+++ core/src/com/mygdx/game/supp/StatisticsCollector.java
@@ -0,0 +1,52 @@
+//package com.mygdx.game.supp;
+//
+//
+//import com.mygdx.game.supp.AnswerButtons.*;
+//
+//import static com.mygdx.game.supp.AnswerButtons.checkForRightAnswer;
+//
+//public class StatisticsCollector {
+//
+//    static int pblScore ;
+//    static int algScore ;
+//    static int oopScore ;
+//
+//    public static void setPblScore() {
+//
+//        if ( CourseProperties.checkForPbl() == true &&  checkForRightAnswer == true  ) {
+//            pblScore+=1;
+//        }
+//    }
+//     public static int getPblScore(){
+//        return pblScore ;
+//     }
+//
+//
+//    public static void setOopScore() {
+//
+//        if ( CourseProperties.checkForPbl() == true &&  checkForRightAnswer == true ) {
+//            pblScore+=1;
+//        }
+//    }
+//    public static int getOopScore(){
+//        return oopScore ;
+//    }
+//
+//    public static void setAlgScore() {
+//
+//        if ( CourseProperties.checkForPbl() == true &&  checkForRightAnswer == true  ) {
+//            algScore+=1;
+//        }
+//    }
+//    public static int getAlgScore(){
+//        return algScore ;
+//    }
+//
+//
+//}
+//
+//// remember to add statistics for right and wrong , to give a percentage for each course
+//
+//
+//
+//
